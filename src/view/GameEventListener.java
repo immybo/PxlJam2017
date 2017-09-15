7ce7d633a3ad8c14435a3e9a2c276a4dbf9ab069
Julian Schurhammer <julian.schurhammer@gmail.com>
2017-09-15 22:17:16 +1200

    gamecontroller

--- src/view/GameEventListener.java
+++ src/view/GameEventListener.java
@@ -1,13 +1,21 @@
 package view;
 
+import model.Level;
+
 public class GameEventListener {
+	private Level level;
+	
+	public GameEventListener(Level level) {
+		this.level = level;
+	}
+	
 	public void onLeftPressed() {
-		
+		level.getPlayer().moveLeft();
 	}
 	public void onRightPressed() {
-		
+		level.getPlayer().moveRight();
 	}
 	public void onJumpPressed() {
-		
+		level.getPlayer().jump();
 	}
 }

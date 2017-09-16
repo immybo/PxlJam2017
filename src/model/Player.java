3695ac551648f4c0830783afb8e8ae7515dab6ff
R. Campbell <robertcampbellnz@gmail.com>
2017-09-16 20:12:05 +1200

    add animation on player and crawling

--- src/model/Player.java
+++ src/model/Player.java
@@ -13,17 +13,35 @@ public class Player extends Character {
 	private static final int MOVEMENT_SPEED = 500;
 	private boolean onGround;
 	private boolean jumpNextTick;
-	private Image texture;
+	private List<Image> walkTextures;
+	private List<Image> crawlTextures;
+	private Image currentTexture;
 	private long lastShotTime;
 	private double shootRate = 0.01;
 
+	private double textureChangeRate = 0.005;
+	private double textureChangeTime;
+	private boolean isCrawling;
+
+	private AABB defaultAABB;
+	private AABB crawlingAABB;
 
 	public Player(AABB aabb, int depth, Level level) {
 		super(aabb, depth, 10, level);
-		this.texture = Textures.PLAYER;
+		this.walkTextures = new ArrayList<Image>();
+		walkTextures.add(Textures.PLAYER_WALK_1); walkTextures.add(Textures.PLAYER_WALK_2);
+		this.crawlTextures = new ArrayList<Image>();
+		crawlTextures.add(Textures.PLAYER_CRAWL_1); crawlTextures.add(Textures.PLAYER_CRAWL_2);
+		this.currentTexture = walkTextures.get(0);
 		this.onGround = true;
 		this.jumpNextTick = false;
 		this.lastShotTime = 0;
+		this.textureChangeTime = System.currentTimeMillis();
+		this.defaultAABB = aabb;
+
+		Vector crawlingCenter = new Vector(defaultAABB.center.x, defaultAABB.center.y + (defaultAABB.get_size().y*0.25));
+		Vector crawlingSize = new Vector(defaultAABB.get_size().x, defaultAABB.get_size().y*0.25);
+		this.crawlingAABB = new AABB(crawlingCenter, crawlingSize, defaultAABB.velocity, defaultAABB.acceleration);
 	}
 
 	public void setOnGround(boolean value) {
@@ -72,12 +90,46 @@ public class Player extends Character {
 	public void render(Graphics g) {
 		Vector min = getAABB().min();
 		Vector ext = getAABB().extents;
-		g.drawImage(texture, (int) min.x, (int) min.y, (int) (2 * ext.x), (int) (2 * ext.y), null);
+		g.drawImage(currentTexture, (int) min.x, (int) min.y, (int) (2 * ext.x), (int) (2 * ext.y), null);
 	}
 
 	@Override
 	public void tick(double dt) {
+		isCrawling = this.getEffects().contains(StatusEffect.BROKEN_LEG);
+
+		AABB currentAABB = this.getAABB();
+		if (isCrawling) {
+			if (currentAABB.get_size().y != crawlingAABB.get_size().y) {
+				Vector newCenter = new Vector(currentAABB.center.x, currentAABB.center.y + crawlingAABB.get_size().y/2);
+				this.setAABB(new AABB(newCenter, crawlingAABB.extents, currentAABB.velocity, currentAABB.acceleration));
+			}
+		} else {
+			if (currentAABB.get_size().y != defaultAABB.get_size().y) {
+				Vector newCenter = new Vector(currentAABB.center.x, currentAABB.center.y - crawlingAABB.get_size().y/2);
+				this.setAABB(new AABB(newCenter, defaultAABB.extents, currentAABB.velocity, currentAABB.acceleration));
+			}
+		}
+
+		if (System.currentTimeMillis()-textureChangeTime > (1.0/textureChangeRate)) {
+			swapTexture();
+			textureChangeTime = System.currentTimeMillis();
+		}
+
 		super.tick(dt);
+	}
+
+	private void swapTexture() {
+		if (isCrawling) {
+			int i = crawlTextures.indexOf(currentTexture) + 1;
+			if (i == crawlTextures.size())
+				i = 0;
+			currentTexture = crawlTextures.get(i);
+		} else {
+			int i = walkTextures.indexOf(currentTexture) + 1;
+			if (i == walkTextures.size())
+				i = 0;
+			currentTexture = walkTextures.get(i);
+		}
 	}
 
 	@Override

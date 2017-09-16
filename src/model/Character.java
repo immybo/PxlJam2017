33302adcbde111d585ef57db6e87601853ca3508
Julian Schurhammer <julian.schurhammer@gmail.com>
2017-09-16 18:58:14 +1200

    working on status effects

--- src/model/Character.java
+++ src/model/Character.java
@@ -1,12 +1,61 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public abstract class Character extends AbstractEntity {
 
 	private double health;
+	private HashMap<StatusEffect, Long> statusTimeouts;
+	private List<StatusEffect> effects;
 
 	public Character(AABB aabb, int depth, double mass, Level level) {
 		super(aabb, depth, mass, level);
 		this.health = this.getMaxHealth();
+		effects = new ArrayList<StatusEffect>();
+		this.statusTimeouts = new HashMap<StatusEffect, Long>();
+		for(StatusEffect s : StatusEffect.values()){
+			this.statusTimeouts.put(s, new Long(0));
+		}
+	}
+
+	public void addStatusEffect(StatusEffect effect) {
+		if (!effects.contains(effect)) {
+			effects.add(effect);
+		}
+		this.statusTimeouts.put(effect, System.currentTimeMillis() + 3000);
+	}
+
+	public void removeStatusEffect(StatusEffect effect) {
+		if (effects.contains(effect)) {
+			effects.remove(effect);
+		}
+	}
+
+	public List<StatusEffect> getEffects() {
+		return effects;
+	}
+	
+	@Override
+	public void tick(double dt) {
+		if (effects.contains(StatusEffect.BUBBLE))
+			this.setVelocity(new Vector(0, this.getVelocity().y));
+		else if (effects.contains(StatusEffect.FROZEN))
+			this.setVelocity(new Vector(this.getVelocity().x, this.getVelocity().y));
+
+		if(effects.contains(StatusEffect.ON_FIRE)) {
+			Vector vel = this.getVelocity();
+			this.setVelocity(new Vector(vel.x*1.5, vel.y));
+			this.damage(dt * 10);
+		}
+		
+		for(StatusEffect s : StatusEffect.values()){
+			if(this.statusTimeouts.get(s) <= System.currentTimeMillis()) {
+				this.removeStatusEffect(s);
+			}
+		}
+		super.tick(dt);
 	}
 
 	public double getHealth(){ return health; }
--- src/model/Player.java
+++ src/model/Player.java
@@ -15,7 +15,6 @@ public class Player extends Character {
 	private boolean onGround;
 	private boolean jumpNextTick;
 	private Image texture;
-	private HashMap<StatusEffect, Long> statusTimeouts;
 	private long lastShotTime;
 	private double shootRate = 0.001;
 	private Movement movement;
@@ -26,42 +25,18 @@ public class Player extends Character {
 			STATIONARY
 	};
 
-	private List<StatusEffect> effects;
 
 	public Player(AABB aabb, int depth, Level level) {
 		super(aabb, depth, 10, level);
 		this.texture = Textures.PLAYER;
-		effects = new ArrayList<StatusEffect>();
 		this.movement = Movement.STATIONARY;
 		this.onGround = true;
 		this.jumpNextTick = false;
-		this.texture = texture;
-		this.statusTimeouts = new HashMap<StatusEffect, Long>();
 		this.lastShotTime = 0;
-		for(StatusEffect s : StatusEffect.values()){
-			this.statusTimeouts.put(s, new Long(0));
-		}
 	}
 
 	public void setOnGround(boolean value) {
 		this.onGround = value;
-	}
-
-	public void addStatusEffect(StatusEffect effect) {
-		if (!effects.contains(effect)) {
-			effects.add(effect);
-		}
-		this.statusTimeouts.put(effect, System.currentTimeMillis() + 3000);
-	}
-
-	public void removeStatusEffect(StatusEffect effect) {
-		if (effects.contains(effect)) {
-			effects.remove(effect);
-		}
-	}
-
-	public List<StatusEffect> getEffects() {
-		return effects;
 	}
 
 	public void shoot(boolean isRight){
@@ -110,31 +85,17 @@ public class Player extends Character {
 	}
 
 	@Override
-	public void tick(double dt){
+	public void tick(double dt) {
 		double ySpeed = this.getVelocity().y;
 		double xSpeed;
-		if (effects.contains(StatusEffect.BUBBLE))
-			xSpeed = 0;
-		else if (effects.contains(StatusEffect.FROZEN))
-			// Retain the same speed
-			xSpeed = this.getVelocity().x;
-		else if(this.movement == Movement.MOVE_RIGHT)
+		if(this.movement == Movement.MOVE_RIGHT)
 			xSpeed = this.MOVEMENT_SPEED * dt;
 		else if(this.movement == Movement.MOVE_LEFT)
 			xSpeed = -this.MOVEMENT_SPEED * dt;
 		else
 			xSpeed = 0;
-		if(effects.contains(StatusEffect.ON_FIRE)) {
-			xSpeed *= 1.5;
-			this.damage(dt * 10);
-		}
 
 		this.setVelocity(new Vector(xSpeed, ySpeed));
-		for(StatusEffect s : StatusEffect.values()){
-			if(this.statusTimeouts.get(s) <= System.currentTimeMillis()) {
-				this.removeStatusEffect(s);
-			}
-		}
 		super.tick(dt);
 	}
 }

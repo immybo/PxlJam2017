b607dfbecb8520cc8cc487b92046e1be49aac9d4
Kai Yin Xu <eskydk@gmail.com>
2017-09-17 16:39:03 +1200

    Level changes so levels 0 - 3

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -21,8 +21,10 @@ import javax.swing.Timer;
 public class Main {
 	public static final String[] LEVELS = {
 			"resources/levels/aLevelOfSorts",
-			"resources/levels/testLevel",
-			"resources/levels/testLevel"
+			"resources/levels/breakLeg.txt",
+			"resources/levels/squish.txt",
+			"resources/levels/bubble.txt"
+			
 	};
 
 	public static void main(String[] args) {
